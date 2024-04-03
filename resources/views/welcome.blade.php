f90e1136f15535a22c3406107e4dc4ee416be509
Md-shefat-masum <mshefat924@gmail.com>
2024-04-03 06:33:48 +0600

    class update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <section>
+        <h1>check</h1>
         <div class="container test " style="display: grid; justify-content: center; align-items: center;height: calc(-115px + 100vh);">
             <img src="/logo-full.png" style="max-width: 300px;" alt="pyramid">
         </div>
